58f6641557afbd58effe0ab4d353ac550d227cc9
Antonio <forzeska@bk.ru>
2021-11-16 03:11:45 +0500

    Добавление функции управления ролями, добавление RoleInitializator

---
+++ EducateApp/Models/RoleInitializer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducateApp.Models
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+
+            string adminEmail = "forzeska@bk.ru";
+            string password = "Qwerty123";
+
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("registeredUser") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("registeredUser"));
+            }
+
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User
+                {
+                    Email = adminEmail,
+                    UserName = adminEmail,
+                    LastName = "Антонов",
+                    FirstName = "Михаил",
+                    Patronymic = "Валерьевич",
+                    EmailConfirmed = true
+                };
+
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
