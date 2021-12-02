ea7e426826424130efe036878cc0efef5320304f
forzeska <assassin.antonov@yandex.ru>
2021-12-02 10:55:40 +0500

    Добавление подтверждения пользователя по почте и таблицы "Формы обучения"

--- EducateApp/Models/AppCtx.cs
+++ EducateApp/Models/AppCtx.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EducateApp.Models.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace EducateApp.Models
-{
+namespace EducateApp.Models {
     public class AppCtx : IdentityDbContext<User>
     {
         public AppCtx(DbContextOptions<AppCtx> options) : base(options)
         {
             Database.EnsureCreated();
         }
+        public DbSet<FormOfStudy> FormsOfStudy { get; set; }
     }
 }
---
+++ EducateApp/ViewModels/FormsOfStudy/CreateFormOfStudyViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducateApp.ViewModels.FormsOfStudy {
+    public class CreateFormOfStudyViewModel {
+        [Required(ErrorMessage = "Введите название формы обучения")]
+        [Display(Name = "Форма обучения")]
+        public string FormOfEdu { get; set; }
+    }
+}
